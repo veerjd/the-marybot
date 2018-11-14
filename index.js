107aebcdca7e31002af34e3cb35ec133cd3e4b4b
JD Veer <veerjd@gmail.com>
2018-11-14 00:31:15 -0500

    .

--- index.js
+++ index.js
@@ -20,4 +20,9 @@ client.on('message', msg => {
   }
 });
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('Listening on ' + port);
+});
+
 client.login(botconfig.token);
