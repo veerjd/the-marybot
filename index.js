31a9a75f92553555a5260b611c047cbf01f2fc73
JD Veer <veerjd@gmail.com>
2019-02-10 19:48:29 -0500

    .

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const botconfig = require('./botconfig.json')
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const express = require('express');
@@ -10,21 +9,24 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if(msg.author.bot) return;
-  if(msg.channel.type === "dm") return;
+  if (!message.content.startsWith(prefix) || message.author.bot || msg.channel.type === "dm") return;
 
-  let prefix = process.env.PREFIX;
-  let messageArray = msg.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+  const args = message.content.slice(prefix.length).split(' ');
+  const cmd = args.shift().toLowerCase();
 
   console.log("prefix:", prefix);
   console.log("cmd:", cmd);
   console.log("args:", args);
 
-  if(cmd === `${prefix}hello`) {
-    return msg.channel.send("Hello!");
+//HELLO
+  if(cmd === "hello") {
+    message.reply("world!");
   }
+//CREATE CHANNEL
+  if(cmd === "project") {
+
+  };
+
 });
 
 const port = process.env.PORT || 5000;
