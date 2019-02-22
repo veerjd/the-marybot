b1d9b953efd08bdfaf31f5e3d6eafec0e1f8009e
JD Veer <veerjd@gmail.com>
2019-02-22 16:26:02 -0500

    .

--- index.js
+++ index.js
@@ -24,7 +24,23 @@ client.on('message', message => {
     message.channel.send("world!")
       .catch(console.error);
   }
-//CREATE CHANNEL
+  //EMBED
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+  //CREATE CHANNEL
   if(cmd === "project" || cmd === "newproject" || cmd === "projet") {    
     let permArray = [];
     let mentionsArray = Object.keys(message.mentions.users);
