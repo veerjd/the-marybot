0d9cf7cd55f512ca94c5cbbe255a895ae5b31be8
JD Veer <veerjd@gmail.com>
2019-02-22 16:37:14 -0500

    .

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { Client, RichEmbed } = require('discord.js');
 const client = new Discord.Client();
 const express = require('express');
 
@@ -26,7 +26,7 @@ client.on('message', message => {
   }
   //EMBED
   // If the message is "how to embed"
-  if (message.content === '?how to embed') {
+  if (cmd === 'embed') {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
