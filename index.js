949c064332b3a94ca2b05f7815ea9537639edc24
JD Veer <veerjd@gmail.com>
2019-02-22 23:34:55 -0500

    .

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).split(' ');
   const cmd = args.shift().toLowerCase();
 
-  console.log("cmd: ", cmd);
-  console.log("args: ", args);
+  console.log("msg: ", message);
+  console.log("parsed msg: ", JSON.parse(message));
 
 //HELLO
   if(cmd === "hello") {
