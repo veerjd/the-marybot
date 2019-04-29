72ad9f818a5aa71393d9ca7cc03890e99f6e823e
JD Veer <veerjd@gmail.com>
2019-04-29 15:31:03 -0400

    .

--- index.js
+++ index.js
@@ -208,8 +208,9 @@ if(event.t === "MESSAGE_REACTION_REMOVE") {
 client.on('message', message => {
 prefix = process.env.PREFIX || botconfig.PREFIX;
 
-if (message.channel.name.includes("annonce") && ()) {
+if (message.channel.name.includes("annonce")) {
     console.log(message.mentions.users);
+    console.log(message.mentions.roles);
 }
 
 //--------------------------------------
