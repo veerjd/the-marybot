e8cf001f2d5987bfbcb4547ace5b142d9ecacd4c
JD Veer <veerjd@gmail.com>
2019-05-03 06:34:47 -0400

    .

--- index.js
+++ index.js
@@ -212,10 +212,16 @@ prefix = process.env.PREFIX || botconfig.PREFIX;
 if(message.author.bot)
     return;
 
+//--------------------------------------
+//         AUTO-RESPONDER TAG
+//--------------------------------------
 if (message.channel.name.includes("annonce")) {
     if(!message.mentions.users.count === undefined && !message.mentions.roles.count === undefined) {
         message.channel.send(`Normalement, il faut mentionner les rôles ou personnes concernées par l'annonce que tu fais, ${message.author}. Je te conseille même de supprimer ton message et le réécrire en taggant les gens et rôles concernés (en utilisant le \`@\`)`)
-            .then(console.log(`Avertissement de mentions envoyé dans ${message.channel.name}`))
+            .then(msg => {
+                console.log(`Avertissement de mentions envoyé dans ${message.channel.name}`);
+                setTimeout(msg.delete(), 6000);
+            })
             .catch(console.error());
     }
 }
